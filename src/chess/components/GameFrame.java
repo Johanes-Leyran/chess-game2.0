9c57ed2d1da85463c247890c057839f6717e7b8f
potato <leyranjohanes@gmail.com>
2026-01-13 22:03:36 +0800

    Added some todos

--- src/chess/components/GameFrame.java
+++ src/chess/components/GameFrame.java
@@ -3,12 +3,16 @@ package chess.components;
 import javax.swing.*;
 import java.awt.*;
 
+// TODO: make it scale base on the monitor's width
+// TODO: make it compatible with full screen mode
+// TODO: learn Swing
+
 public class GameFrame extends JFrame {
     public void loadUI() {
         this.setTitle("Chess Game 2.0");
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(1000, 700));
+        this.setPreferredSize(new Dimension(1000, 800));
         this.pack();
     }
 }
